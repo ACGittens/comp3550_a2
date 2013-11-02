61415b41ec33397cf4af679d85560f919fa4a259
Rikaard Hosein <rikaard.hosein@gmail.com>
2013-11-02 00:48:53 -0400

    User code is pretty much complete, testing for bugs

--- user_test.php
+++ user_test.php
@@ -11,11 +11,14 @@ require_once('phpassPasswordHasher.php');
 User::set_password_hasher( new phpassPasswordHasher );
 User::set_user_manager( new MySQLUserManager() );
 
-$u = User::get_user_by_id(16);
+$u = new User("cherlton@gay.com","Cherlton","Milette","passwords_suck");
+$u->save();
 
-echo $u->print_details();
-$res = $u->check_password("testpassword");
-echo (( $res === FALSE ) ? "Password comparison failed!\n": "Password comparison success!\n");
-$res = $u->check_password("testpass");
-echo (( $res === FALSE ) ? "Password comparison failed!\n": "Password comparison success!\n");
+//$u = User::get_user_by_id(16);
+//
+//echo $u->print_details();
+//$res = $u->check_password("testpassword");
+//echo (( $res === FALSE ) ? "Password comparison failed!\n": "Password comparison success!\n");
+//$res = $u->check_password("testpass");
+//echo (( $res === FALSE ) ? "Password comparison failed!\n": "Password comparison success!\n");
 ?>
