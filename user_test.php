06544dfc0426f9f32d6408a603a31edaeb24b063
Rikaard Hosein <rikaard.hosein@gmail.com>
2013-11-01 23:54:34 -0400

    User code is pretty much complete, testing for bugs

--- user_test.php
+++ user_test.php
@@ -13,7 +13,7 @@ User::set_user_manager( new MySQLUserManager() );
 
 $u = User::get_user_by_email("rikaard1993@hotmail.com");
 
-printf("Id: %d\nEmail: %s\nHash: %s\nFirst Name: %s\nLast Name: %s\n\n",
+printf("Id: %s\nEmail: %s\nHash: %s\nFirst Name: %s\nLast Name: %s\n\n",
       $u->get_id(), $u->get_email(), $u->get_hash(), $u->get_first_name(), $u->get_last_name());
 
 
