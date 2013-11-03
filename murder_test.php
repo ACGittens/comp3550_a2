1a9dd40fb5a36c3175eef7c8e8f51a51dfff9268
Rikaard Hosein <rikaard.hosein@gmail.com>
2013-11-03 13:42:44 -0500

    testing murdermanager

--- murder_test.php
+++ murder_test.php
@@ -4,6 +4,8 @@ require_once('./Models/Murder.php');
 require_once('MySQLMurderManager.php');
 
 
+Murder::set_murder_manager( new MySQLMurderManager );
+
 $m = new Murder('jarred',20,'flatland','knife','multiple stab wounds');
 $m->save();
 
