8b0ad36d2e6848fd5ad34faf4d64ca3fdcaaaeab
Rikaard Hosein <rikaard.hosein@gmail.com>
2013-11-03 15:17:48 -0500

    working on SQLMurderManager

--- murder_test.php
+++ murder_test.php
@@ -13,7 +13,7 @@ Murder::set_murder_manager( new MySQLMurderManager );
 $murder_results = Murder::get_murders_by_name("jarred");
 foreach( $murder_results as $res )
 {
-	echo $res->get_details()."\n";
+	echo $res->test()."\n";
 }
 
 ?>
